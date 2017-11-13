81036459168d0e426e26fec0666e81b2e9bb9536
Pablo Jimenez Casado <pjimenez.casado@atsistemas.com>
2017-11-13 19:40:13 +0100

    Add first function: initGame

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,33 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+admin.initializeApp(functions.config().firebase);
+
+exports.initGame = functions.database.ref('/juegos/{idJuego}/jugadores').onWrite(event => {
+	
+ 	const juegoRef = event.data.ref.parent;
+ 	const numJugadores = juegoRef.child('config').child('numJugadores');
+ 	const jugadoresRef = event.data.ref;
+ 	const juegoStateRef = juegoRef.child('estado');
+ 	let number = 1;
+ 	numJugadores.once("value", function(data) {
+ 		number = data.val()
+	});
+	
+    return jugadoresRef.once("value", (snapshot) => {
+     	console.log(snapshot.numChildren())
+
+     	if (snapshot.numChildren() === number ) {
+     		juegoRef.update({
+     			"estado":"1"});
+     		console.log("Empieza la partida");
+     	}else {
+     		juegoRef.update({
+     			"estado":"0"});
+     		console.log('Faltan jugadores');
+     	}
+    });
+});
+
+
